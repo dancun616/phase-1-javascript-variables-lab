1b2c5523e337a8e8aa0c4ca660105b58895049cd
DANCUN ODHIAMBO <dancun616@gmail.com>
2022-11-23 17:06:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+
+const companyName= "Scuber";
+console.log(companyName);
+let mostProfitableNeighborhood= "Chelsea";
+console.log(mostProfitableNeighborhood);
+let companyCeo= "Susan Smith";
+console.log(companyCeo);
+white_check_mark
+eyes
+raised_hands
+
+function scuberGreetingForFeet(distance){
+  if (distance <= 400 ) {
+    return'This one is on me!';
+  } else if (distance >= 2000 && distance < 2500) {
+    return'I will gladly take your thirty bucks.';
+  } else (distance > 2500); {
+    return'No can do.';
+  }
+}
